63c866816445952f638eb177eb93f030654135bf
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2020-11-09 15:03:29 +0530

    Update add-tools.sh

--- add-tools.sh
+++ add-tools.sh
@@ -1,3 +1,3 @@
-docker build -t ssj:latest .
+docker build -t ssj:latest . && \
+echo 'xhost +local:root && docker run --rm --shm-size=4g --workdir=/root --hostname=ssj --net=host --privileged -e DISPLAY -v $HOME/.ssj:/root ssj:latest terminator && xhost -local:root' > $HOME/.local/bin/ssj
 docker image rm $(docker images -q --filter "dangling=true") &>/dev/null
-sudo /bin/bash -c "echo 'xhost +local:root && docker run --rm --shm-size=4g --workdir=/root --hostname=ssj --net=host --privileged -e DISPLAY -v /home/ssj:/root ssj:latest terminator && xhost -local:root' > /usr/local/bin/ssj"
